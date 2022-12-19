cad22a1e534bb68aac8348bd240994e9a3d3f1c6
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-12-19 15:28:13 -0300

    #novo commit

--- src/main/java/com/tuyo/tuyofood/api/controller/KitchenController.java
+++ src/main/java/com/tuyo/tuyofood/api/controller/KitchenController.java
@@ -1,22 +1,18 @@
 package com.tuyo.tuyofood.api.controller;
 
-import com.tuyo.tuyofood.domain.entity.Kitchen;
-import com.tuyo.tuyofood.domain.repository.KitchenRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.tuyo.tuyofood.domain.entity.*;
+import com.tuyo.tuyofood.domain.repository.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 /* GET /kitchens HTTP/1.1
 *  1. @Controller: Indica que é uma Classe Component do tipo Controlador
 *  2. @RequestMapping: mapear o controlador com uma URI: /kitchens
 *  3. @ResponseBody: indica que a resposta dos métodos desse controlador devem ir para
 * a resposta da requisição HTTP. Ou seja, a resposta do método listar, deve listar as cozinhas
-*  4. @RestController: substitui @Controller e @ResponseBody, cumprindo assim, ambas necessidades
+*  4. @RestController: substitui @Controller e private final, cumprindo assim, ambas necessidades
 * e indica que é um Controller Rest */
 
 // @Controller
